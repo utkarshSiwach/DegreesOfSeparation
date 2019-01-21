819c83a6b077c0ec5b90195b77e8bdc59cca646d
Timo Kersten <kersten@in.tum.de>
2019-01-21 10:13:52 +0100

    Initial import.

---
+++ src/DistCalculator.cpp
@@ -0,0 +1,17 @@
+#include "DistCalculator.hpp"
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+DistCalculator::DistCalculator(std::string edgeListFile)
+{
+   // TODO: implement graph parsing here
+}
+
+int64_t DistCalculator::dist(Node a, Node b)
+{
+   // TODO: implement distance calculation here
+   return -1;
+}
